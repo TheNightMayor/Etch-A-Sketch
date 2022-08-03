302bce6629c8a058b2a94b07fc02025dd0eaaf9a
Lawrence LeBlanc <lawrencekeeganleblanc@gmail.com>
2022-08-03 12:49:38 -0500

    made grid

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+function drawGrid(length, height) {
+  for (let index = 0; index < height; index++) {
+    drawRow(length);
+  }
+  function drawRow(length) {
+    let grid = document.getElementById("grid");
+    const row = document.createElement("div");
+    row.classList.add("row");
+    grid.appendChild(row);
 
-function createSquare() {
-   const square = document.createElement("div");
-   square.classList.add('square');
-   let grid = document.getElementById("grid");
-   grid.appendChild(square);
+    for (let index = 0; index < length; index++) {
+      drawSquare();
+    }
+    function drawSquare() {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+    }
+  }
 }
-createSquare();
+drawGrid(16, 16);
